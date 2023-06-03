c80be4775165bbd3cbffa789cc4f338507d39b32
Evere77 <116045038+Evere77@users.noreply.github.com>
2023-06-03 13:48:51 -0700

    Initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main (String [] args) {
+        System.out.println("Everett");
+        System.out.println("40");
+        System.out.println("Placentia");
+    }
+}
